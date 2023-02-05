c0126a4c1ff75c0f5fe1fbc956a52d77a683b9f4
esakiryota <esaki1217@gmail.com>
2023-02-05 22:06:40 +0900

    second commit

--- src/navigation.js
+++ src/navigation.js
@@ -41,7 +41,7 @@ function ResponsiveDrawer(props) {
       <Toolbar />
       <Divider />
       <List>
-        {[['プロフィール', '/'], ['スキル', '/skill'], ['作品', '/works']].map((value, index) => (
+        {[['プロフィール', ''], ['スキル', 'skill'], ['作品', 'works']].map((value, index) => (
           <ListItem key={value[0]} disablePadding  >
             <ListItemButton href={value[1]} selected={selectedIndex === 0}
           onClick={(event) => handleListItemClick(event, 0)}>
